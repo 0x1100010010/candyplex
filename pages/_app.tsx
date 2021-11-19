b17a427b484348303b7f8299027e5e145008de4a
bashforger <bash.forger@gmail.com>
2021-11-19 13:08:03 +0500

    Added related libraries

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,11 @@ import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <RecoilRoot><Component {...pageProps} /></RecoilRoot>
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
 
 export default MyApp
